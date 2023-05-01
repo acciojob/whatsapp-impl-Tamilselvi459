9875dd177f50b6d45fac645141a72aa6b938937a
Tamilselvi459 <tamilselvig459@gmail.com>
2023-05-01 11:43:24 +0530

    code done

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -71,11 +71,12 @@ public class WhatsappRepository {
         if(groupUserMap.containsKey(group)){
             List<User> us =  groupUserMap.get(group);
             for(User ua: us){
-                if(ua.getName().equals(sender.getName())){
+                if(ua.getName().equals(sender.getName()) ){
+                    if(groupMessageMap.containsKey(group)) {
                     List<Message> mess = new ArrayList<>();
                     mess.add(message);
                     groupMessageMap.put(group , mess);
-                    return mess.size()-1;
+                    return mess.size()-1; }
 
                 }
 
